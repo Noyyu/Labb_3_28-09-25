f5bba63433aa723e2cea3982fb9354992e57cd7f
Nikki <Nikki.Norberg@hotmail.com>
2025-09-08 14:46:22 +0200

    Added funktionality for the pyramid

---
+++ Labb_3_28-09-25/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Labb_3_28_09_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            int pyramidHeight = 10;
+            int spaces = -1;
+            int stars = -1;
+
+
+            //height
+            for (int i = 1; i < pyramidHeight + 1; i++)
+            {
+                Console.WriteLine();
+                //Width Mellanslag
+                for (int j = 0; j < (spaces = (pyramidHeight - i)); j++)
+                {
+                    Console.Write(" ");
+                }
+                //width stars
+                for (int k = 0; k < (stars = (2 * i - 1)); k++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
